ac49f39e5a7c1c5d76eb052ad5510e3aed1d6348
wholood <wholood@gmail.com>
2023-05-17 23:18:50 -0400

    hay fe

---
+++ proyecto_alpha4.cpp
@@ -0,0 +1,410 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+class Dispositivo; //Cabecera para que no se glichee
+
+class Relacion{
+public: 
+    int ping;
+    string tipo;
+    Dispositivo *con_quien; //Con que dispositivo existe la conexion
+    Relacion *siguiente_R; //Puntero para la lista de relaciones de cada dispositivo
+
+    Relacion(){
+        ping=0;
+        con_quien=NULL;
+        siguiente_R=NULL;
+    };
+};
+
+class Dispositivo{
+public:
+    string hostname;
+    string ip;
+    int cont_relacion; //Contador de relaciones existentes
+    Relacion *lista_vecinos; //Literalmente lista de las conexiones
+    Dispositivo *siguiente_D; //Puntero para la lista de dispositivos del main
+public:
+    Dispositivo(){
+        cont_relacion=0; //Por si acaso
+        lista_vecinos=NULL;
+    };
+    void conectar (int ping_1, string tipo_1, Dispositivo *pana){
+        //Crea una nueva relacion en la lista de relaciones del dispositivo.
+
+        Relacion *nueva_Relacion = new Relacion(); 
+        nueva_Relacion->ping =ping_1;
+        nueva_Relacion->tipo =tipo_1;
+
+        nueva_Relacion->con_quien =pana;
+
+        Relacion *aux1 = lista_vecinos; //Variable auxiliar para el ordenamiento
+        Relacion *aux2;
+
+        //ESTA PENDIENDTE EL ORDENAMIENTO POR TIPO
+        while((aux1!=NULL)&&(aux1->ping <ping_1)){ //Verificar si hay algun ping menor al de la nueva relacion y establecer el auxiliar para agregar el elemento en medio
+            aux2=aux1;
+            aux1=aux1->siguiente_R;
+        }
+
+        if(lista_vecinos == aux1){ //Insertar al inicio, es decir no entro en el while
+            lista_vecinos = nueva_Relacion; //Establece la relacion al inicio de la lista
+        }
+        else{
+            aux2->siguiente_R = nueva_Relacion; //Establece la nueva relacion en medio de la lista
+        }
+
+        nueva_Relacion->siguiente_R=aux1; //se cumple siempre.
+        cont_relacion++; //Incremento el contador de relacion.
+
+        cout<<"Conexion establecida con "<<pana->hostname<<"."<<endl; //mensajito jiji
+    };
+};
+
+
+///--------------------------------------------------------///
+void insertarlista(Dispositivo *&lista, string nombre, string direccion){
+
+    Dispositivo *nuevo_Dispositivo = new Dispositivo();
+    nuevo_Dispositivo->hostname =nombre;
+    nuevo_Dispositivo->ip =direccion;
+
+    Dispositivo *aux1 = lista;
+    Dispositivo *aux2;
+
+    while((aux1!=NULL)&&(aux1->hostname <nombre)){ //Verificar si hay alguna ip menor a n y establecer el auxiliar para agregar el elemento en medio
+        aux2=aux1;
+        aux1=aux1->siguiente_D;
+    }
+
+    if(lista ==aux1){ //Insertar al inicio, es decir no entro en el while
+        lista = nuevo_Dispositivo;
+    }
+    else{
+        aux2->siguiente_D = nuevo_Dispositivo;
+    }
+
+    nuevo_Dispositivo->siguiente_D=aux1; //se cumple siempre.
+    cout<<"Valor ingresado "<<nombre<<"."<<endl;
+}
+
+void mostrarlista(Dispositivo *lista){
+    Dispositivo *actual = lista;
+    //actual = lista;
+    while(actual != NULL){
+        cout<<"("<<actual->ip<<"|"<<actual->hostname<<")-> ";
+        actual= actual->siguiente_D;
+    }
+};
+void mostrarrelacion(Relacion *lista){
+    Relacion *actual = lista;
+    //actual = lista;
+    while(actual != NULL){
+        cout<<"("<<actual->ping<<"|"<<actual->tipo<<"|"<<actual->con_quien->hostname<<")-> ";
+        actual= actual->siguiente_R;
+    }
+};
+
+Dispositivo* buscardispositivo(Dispositivo *lista, string busqueda){
+    //Dentro de lista dispositivo buscar el string del hostname o ip
+	Dispositivo *actual = lista; //Auxiliar para no romper nada
+
+	while ((actual !=NULL)&&(actual->hostname != busqueda)&&(actual->ip != busqueda)){
+		actual = actual->siguiente_D; //Voy recorriendo la lista siempre y cuando no consiga nada hasta el final (NULL)
+	}
+    if(actual==NULL) return NULL;
+
+    return ((actual->hostname==busqueda)||(actual->ip==busqueda))? actual : NULL;
+    //Retorno *actual si lo encontre, si no encontre nada retorno el puntero en NULL
+};
+
+bool verificar_relacion (Dispositivo *h1, Dispositivo *h2){
+    Relacion *actual = h1->lista_vecinos; //Auxiliar pa no romper nada
+
+    while ((actual !=NULL)&&(actual->con_quien!=h2)){ //Recorro la lista de relaciones del dispositivo mientras no consiga h2 hasta el final
+		actual = actual->siguiente_R;
+	}
+
+    return (actual == NULL)? true : false;
+    //Si llego al final de la lista retorno que no hay relacion existente (TRUE) si no llegue al final consegui h2 por ende (FALSE)
+};
+
+void establecer_conexion(int ping_1, string tipo_1, string h_1, string h_2, Dispositivo *listado){
+    Dispositivo *host1 = buscardispositivo(listado,h_1); //Dado un nombre (host o ip) consigo la direccion del dispositivo en la lista
+    Dispositivo *host2 = buscardispositivo(listado,h_2);
+
+    if (host1!=NULL && host2!=NULL && host1!=host2){//Mientras los apuntadores no sean nulos, si son nulos no existe el dispositivo
+        if(verificar_relacion(host1,host2)){ //Verifica que no exista relacion entre ambos dispositivos
+            if(host1->cont_relacion<12 && host2->cont_relacion<12){ //Verifica que tengan espacio para conexion
+                host1->conectar(ping_1,tipo_1,host2); //Establezco la relación en cada uno de los dispositivos
+                host2->conectar(ping_1,tipo_1,host1);    
+            }
+        }
+    }
+    return;
+};
+///-------------------------------------------------------///
+
+void eliminarDispositivo (Dispositivo *&lista, string nombre){
+    if(lista!=NULL){ //Por si acaso
+        Dispositivo  *aux_borrar = buscardispositivo(lista,nombre);
+        Dispositivo  *anterior = lista;
+        
+        if(anterior==aux_borrar){
+            lista=aux_borrar->siguiente_D;
+        }
+        else{
+            while ((aux_borrar != NULL)&&(anterior->siguiente_D != aux_borrar)){
+                anterior=anterior->siguiente_D; //Voy recorriendo la lista siempre y cuando no consiga nada hasta el final (NULL)
+            }
+            anterior->siguiente_D=aux_borrar->siguiente_D;
+        }
+        //respaldar el dispositivo a borrar
+        delete aux_borrar;
+    }
+}
+
+void eliminarRelacion (Relacion *&lista, string nombre){ //PPPPPPPPPPENDIENTEEEEEEEEEEEmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    if(lista!=NULL){ //Por si acaso
+        Relacion  *aux_borrar ;//BUSCARRELACION
+        Relacion  *anterior = lista;
+        
+        //Modificacion de la lista de relaciones del dispositivo
+        if(anterior==aux_borrar){
+            lista=aux_borrar->siguiente_R;
+        }
+        else{
+            while ((aux_borrar != NULL)&&(anterior->siguiente_R != aux_borrar)){
+                anterior=anterior->siguiente_R; //Voy recorriendo la lista siempre y cuando no consiga nada hasta el final (NULL)
+            }
+            anterior->siguiente_R=aux_borrar->siguiente_R;
+        }
+        //respaldar la relacion a borrar
+        delete aux_borrar;
+    }
+}
+
+///-------------------------------------------------------/// BUSQUEDA DE RUTAS DIABOLICAS
+struct Nodo {
+        Dispositivo *dispositivo;
+        Cola *Solu;
+        Nodo *siguiente;
+    };
+class Cola {
+public:
+    Nodo *frente;
+    Nodo *final;
+    int aux_ping;
+
+    Cola(){ //Constructor para la cola vacia
+        frente=NULL; 
+        final=NULL;
+        aux_ping=0;
+    }
+
+    bool vacia() { //Chequea si la cola no tiene elementos
+        return frente == NULL;
+    }
+
+    void agregar_dispCola(Dispositivo *dispositivo) {
+        Nodo *nuevo = new Nodo(); //Crea un nodo
+        nuevo->dispositivo=dispositivo; //Le asigna el apuntador al dispositivo
+        nuevo->siguiente=NULL; //Establece como null el siguiente (fin de cola)
+
+        if (final != NULL) {
+            final->siguiente = nuevo; //Añadimos a nuevo atras del ultimo de la cola
+        }
+        final = nuevo; //Nuevo será el final de la cola
+        if (frente == NULL) {
+            frente = nuevo; //Si la cola esta vacia entonces nuevo va a ser el principio y el fin
+        }
+    }
+    void agregar_colaCola(Cola *cola) {
+        Nodo *nuevo = new Nodo(); //Crea un nodo
+        nuevo->Solu=cola; //Le asigna el apuntador al dispositivo
+        nuevo->siguiente=NULL; //Establece como null el siguiente (fin de cola)
+
+        if (final != NULL) {
+            final->siguiente = nuevo; //Añadimos a nuevo atras del ultimo de la cola
+        }
+        final = nuevo; //Nuevo será el final de la cola
+        if (frente == NULL) {
+            frente = nuevo; //Si la cola esta vacia entonces nuevo va a ser el principio y el fin
+        }
+    }
+
+
+    void sacar_cola() {
+        if (frente != NULL) { //Si el frente NO es nulo
+            Nodo *aux = frente; //Creo un auxiliar al elemento
+            frente = frente->siguiente; //Establezco como primero al que iba atras del del frente
+            if (frente == NULL) { //Si solo estaba el elemento entonces todo es nulo
+                final = NULL;
+            }
+            delete aux; //Elimino el elemento
+        }
+    }
+
+    Dispositivo *quien_al_frente() {
+        return frente->dispositivo; //Me dice que dispositivo esta al frente de la cola
+    }
+    Cola *solucion_al_frente(){
+        return frente->Solu; //Me dice cual es la cola de relaciones que esta al frente.
+    }
+};
+
+Cola Soluciones;
+
+void buscarRutas(Dispositivo *origen, Dispositivo *objetivo) {
+    Cola Solucion; // Cola que almacena las soluciones (rutas)
+    Cola Ruta; // Cola auxiliar para almacenar cada ruta
+
+    Ruta.agregar_dispCola(origen); // Agregar el dispositivo origen a la ruta inicial
+    Ruta.aux_ping = 0; // Inicializar el valor aux_ping en 0
+
+    Solucion.agregar_colaCola(&Ruta); // Agregar la ruta inicial a la cola de soluciones
+
+    while (!Solucion.vacia()) { // Mientras haya soluciones pendientes en la cola
+        Cola rutaActual = *Solucion.frente->Solu; // Obtener la ruta actual de la cola de soluciones
+        Dispositivo *dispositivoActual = rutaActual.quien_al_frente(); // Obtener el dispositivo actual de la ruta
+
+        if (dispositivoActual == objetivo) { // Si se llegó al dispositivo objetivo
+            // Imprimir la ruta y su valor aux_ping
+            Nodo *nodoRuta = rutaActual.frente;
+            while (nodoRuta != NULL) {
+                cout << nodoRuta->dispositivo->hostname << " -> ";
+                nodoRuta = nodoRuta->siguiente;
+            }
+            cout << "Valor aux_ping: " << rutaActual.aux_ping << endl;
+        }
+
+        // Obtener las relaciones del dispositivo actual
+        Relacion *relacionActual = dispositivoActual->lista_vecinos;
+        while (relacionActual != NULL) {
+            if (!existeEnRuta(relacionActual->con_quien, &rutaActual)) { // Evitar bucles en la ruta
+                Cola nuevaRuta = rutaActual; // Crear una copia de la ruta actual
+                nuevaRuta.agregar_dispCola(relacionActual->con_quien); // Agregar el siguiente dispositivo a la nueva ruta
+                nuevaRuta.aux_ping += relacionActual->ping; // Actualizar el valor aux_ping de la nueva ruta
+                Solucion.agregar_colaCola(&nuevaRuta); // Agregar la nueva ruta a la cola de soluciones
+            }
+            relacionActual = relacionActual->siguiente_R;
+        }
+
+        Solucion.sacar_cola(); // Sacar la ruta actual de la cola de soluciones
+    }
+}
+
+bool existeEnRuta(Dispositivo *dispositivo, Cola *ruta) {
+    Nodo *nodoRuta = ruta->frente;
+    while (nodoRuta != NULL) {
+        if (nodoRuta->dispositivo == dispositivo) {
+            return true;
+        }
+        nodoRuta = nodoRuta->siguiente;
+    }
+    return false;
+}
+void imprimirRutas(Dispositivo *origen, Dispositivo *objetivo) {
+    Cola Solucion; // Cola que almacena las soluciones (rutas)
+    Cola Ruta; // Cola auxiliar para almacenar cada ruta
+
+    Ruta.agregar_dispCola(origen); // Agregar el dispositivo origen a la ruta inicial
+    Ruta.aux_ping = 0; // Inicializar el valor aux_ping en 0
+
+    Solucion.agregar_colaCola(&Ruta); // Agregar la ruta inicial a la cola de soluciones
+
+    while (!Solucion.vacia()) { // Mientras haya soluciones pendientes en la cola
+        Cola rutaActual = *Solucion.frente->Solu; // Obtener la ruta actual de la cola de soluciones
+        Dispositivo *dispositivoActual = rutaActual.quien_al_frente(); // Obtener el dispositivo actual de la ruta
+
+        if (dispositivoActual == objetivo) { // Si se llegó al dispositivo objetivo
+            // Imprimir la ruta y su valor aux_ping
+            Nodo *nodoRuta = rutaActual.frente;
+            while (nodoRuta != NULL) {
+                cout << nodoRuta->dispositivo->hostname << " -> ";
+                nodoRuta = nodoRuta->siguiente;
+            }
+            cout << "Valor aux_ping: " << rutaActual.aux_ping << endl;
+        }
+
+        // Obtener las relaciones del dispositivo actual
+        Relacion *relacionActual = dispositivoActual->lista_vecinos;
+        while (relacionActual != NULL) {
+            if (!existeEnRuta(relacionActual->con_quien, &rutaActual)) { // Evitar bucles en la ruta
+                Cola nuevaRuta = rutaActual; // Crear una copia de la ruta actual
+                nuevaRuta.agregar_dispCola(relacionActual->con_quien); // Agregar el siguiente dispositivo a la nueva ruta
+                nuevaRuta.aux_ping += relacionActual->ping; // Actualizar el valor aux_ping de la nueva ruta
+                Solucion.agregar_colaCola(&nuevaRuta); // Agregar la nueva ruta a la cola de soluciones
+            }
+            relacionActual = relacionActual->siguiente_R;
+        }
+
+        Solucion.sacar_cola(); // Sacar la ruta actual de la cola de soluciones
+    }
+}
+
+
+
+/*void back(Dispositivo *actual, Dispositivo *objetivo, Cola *soluciones, int nivel){
+    if(nivel > actual->cont_relacion) return; //llegar al numero máximo de relacion
+
+    Cola Ruta;
+    Ruta.agregar_dispCola(actual);
+
+    for(Dispositivo *auxiliar; auxiliar!=NULL;){
+        auxiliar=actual->lista_vecinos->con_quien;
+
+        if(auxiliar->lista_vecinos->con_quien != objetivo){
+            if(auxiliar->lista_vecinos->con_quien == actual){
+                auxiliar=auxiliar->lista_vecinos->siguiente_R->con_quien;
+            }
+        }
+        else{
+            Ruta.agregar_dispCola(auxiliar);
+            return;
+        }   
+    }
+    
+    
+    
+
+};*/
+
+///-------------------------------------------------------/// 
+int main (){
+    Dispositivo *lista = NULL;
+    string name[14]={"a","b","c","d","e","f","g","h","i","k","l","m","n","o"};
+    string dire[14]={"10","17","15","12","05","11","18","16","13","06","14","25","19","01"};
+    for(int i=0; i<14; i++){
+        //cout<<"Ingresa valor: \t"; 
+        //cin>>valor;
+        insertarlista(lista,name[i],dire[i]);
+    }
+    establecer_conexion(1,"f","a","b",lista);
+    establecer_conexion(2,"f","a","c",lista);
+    establecer_conexion(3,"f","a","g",lista);
+    establecer_conexion(4,"f","b","c",lista);
+    establecer_conexion(5,"f","c","g",lista);
+
+    mostrarrelacion(buscardispositivo(lista,"a")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"b")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"c")->lista_vecinos);cout<<endl;
+    /*mostrarrelacion(buscardispositivo(lista,"d")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"e")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"f")->lista_vecinos);cout<<endl;*/
+    mostrarrelacion(buscardispositivo(lista,"g")->lista_vecinos);cout<<endl;
+    /*mostrarrelacion(buscardispositivo(lista,"h")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"i")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"k")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"l")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"m")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"n")->lista_vecinos);cout<<endl;
+    mostrarrelacion(buscardispositivo(lista,"o")->lista_vecinos);cout<<endl;*/
+
+
+    cout<<endl;
+    mostrarlista(lista);
+
+    return 0;
+}
